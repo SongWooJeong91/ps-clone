d2d1fa0bd75f7b8faf605ed20b02b7be5a701a5d
SongWooJeong91 <th171717@hanmail.net>
2022-06-24 03:17:52 +0900

    'storeInfo'

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,11 @@
 const conTran = document.querySelector('.con__tran'); // 콘텐츠 타이틀
 const conTranProduct = document.querySelector('.con__tran-product');
 const conList = document.querySelectorAll('.con__card>li'); // 콘텐츠 카드
-console.log('conTranProduct : ', conTranProduct);
+// console.log('conTranProduct : ', conTranProduct);
 
 // 스크롤 애니메이션 함수
 const scrollAni = (ele, conY, opacityNum, transitionOp) => {
-  console.log('scrollAin 실행');
+  // console.log('scrollAin 실행');
   ele.style.transform = `translateY(${conY}px)`;
   ele.style.opacity = opacityNum;
   ele.style.transition = `all ${transitionOp}s ease-in-out`;
@@ -13,34 +13,35 @@ const scrollAni = (ele, conY, opacityNum, transitionOp) => {
 
 window.addEventListener('scroll', () => {
   const scrY = window.scrollY; // 현재 스크롤 Y좌표
-  console.log('스크롤Y 위치:', scrY);
+  // console.log('스크롤Y 위치:', scrY);
   let conY, opacityNum, transitionOp;
   if (scrY < 550) {
     conY = 180; // translateY
     opacityNum = 0; // opacity
     transitionOp = 1.4; // trasition time
     scrollAni(conTran, conY, opacityNum, transitionOp);
-  } else if (scrY > 550) {
+  } else if (scrY < 680) {
     conY = 0;
     opacityNum = 1;
     transitionOp = 1.4;
     scrollAni(conTran, conY, opacityNum, transitionOp);
-  }
-  if (scrY < 680) {
     conList.forEach((item, index) => {
       conY = 180;
       opacityNum = 0;
       transitionOp = 0.8 + index * 0.3;
       scrollAni(item, conY, opacityNum, transitionOp);
     });
-  } else if (scrY > 680) {
+  } else if (scrY < 1380) {
     conList.forEach((item, index) => {
       conY = 0;
       opacityNum = 1;
       transitionOp = 0.8 + index * 0.3;
       scrollAni(item, conY, opacityNum, transitionOp);
     });
-  } else if (scrY < 1380) {
-    console.log('스크롤 실행ㅇ');
+  } else if (scrY < 1500) {
+    conY = 0;
+    opacityNum = 1;
+    transitionOp = 1.4;
+    scrollAni(conTranProduct, conY, opacityNum, transitionOp);
   }
 });
--- js/slider.js
+++ js/slider.js
@@ -25,3 +25,39 @@ next.addEventListener('click', () => {
   else currentIdx += 1;
   imgPosition();
 });
+
+// 매장 안내 슬라이드
+const conStorePrevBtn = document.querySelector('.con__store-prevBtn');
+const conStoreNextBtn = document.querySelector('.con__store-nextBtn');
+const conStoreSlideview = document.querySelector('.con__store-slideview'); // 슬라이드 뷰
+const conStoreUl = document.querySelector('.con__store-ul');
+const conStoreUlLi = document.querySelectorAll('.con__store-ul>li');
+
+let firstSlide = conStoreUl.firstElementChild;
+let lastSlide = conStoreUl.lastElementChild;
+let clonedFirst = firstSlide.cloneNode(true);
+let clonedLast = lastSlide.cloneNode(true);
+console.log(clonedFirst);
+
+conStoreUl.appendChild(clonedFirst);
+conStoreUl.insertBefore(clonedLast, conStoreUl.firstElementChild);
+
+// 전체 화면너비에서 -100
+let autoIdx = 1;
+const lastIdx = conStoreUlLi.length; // li 갯수
+
+console.log(lastIdx);
+
+// slideShow();
+function slideShow() {
+  if (autoIdx === lastIdx) autoIdx = 0;
+  conStoreUl.style.transform = `translateX(-${(imgSize - 100) * autoIdx}px)`;
+  conStoreUl.style.transition = 'transform 0.7s ease-in-out';
+  autoIdx++;
+  setTimeout(slideShow, 2000);
+}
+
+// function autoSlide() {
+//   conStoreUl.style.transform = `translateX(-${(imgSize - 100) * autoIdx}px)`;
+//   autoIdx++;
+// }
